552dab65f5fa31f52cff429b19c2a6416d35c291
laosongRuning <2621990795@qq.com>
2020-04-22 16:01:51 +0800

    地图v3.0修改

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import './index.scss'
+import { NavBar, Icon } from 'antd-mobile';
+class Map extends React.Component {
+    componentDidMount() {
 
-class Map extends React.Component{
-    render () {
-        return(
-            <div>地图</div>
+    }
+    // 初始化地图
+    initMap = () => {
+        // 创建地图实例
+        //  地图定位的经纬度设置(天安门)
+        // 地图初始化，同时设置地图展示级别
+        const { BMap } = window;
+        const map = new BMap.Map("container");
+        var point = new BMap.Point(116.404, 39.915);
+        map.centerAndZoom(point, 15);
+    }
+
+
+
+
+    render() {
+        return (
+            <div className="mapBox">
+                {/* 导航栏 */}
+                <NavBar
+                    mode="light"
+                    icon={<Icon type="left" />}
+                    onLeftClick={() => {
+                        this.props.history.goBack()
+                    }}
+                >
+                地图找房
+                </NavBar>
+                {/* 地图 */}
+                <div id="container"></div>
+            </div>
         )
     }
 }
