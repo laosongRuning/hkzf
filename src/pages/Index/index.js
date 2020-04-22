6994817540c531eeec921265e55ac72df8d17633
laosongRuning <2621990795@qq.com>
2020-04-22 11:42:11 +0800

    Index顶部搜索、宫格渲染、列表渲染，以及收据api和接口的封装

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,22 +1,52 @@
 import React from 'react'
-import { Carousel, Flex } from 'antd-mobile'
+import { Carousel, Flex, Grid, WingBlank,SearchBar } from 'antd-mobile'
 import { BASE_URL } from '../../utils/axios'
-import { getSwiper } from '../../utils/api/Home'
+import { getSwiper, getGroup, getNews } from '../../utils/api/Home'
 // 导入栏目导航的数据
 import navs from '../../utils/navs'
-import './index.css'
+import './index.scss'
 // import axios from 'axios'
 class Index extends React.Component {
   state = {
+    // 咨询列表数据
+    news: [],
+    // 宫格数据
+    groupListData: [],
     // 走马灯数据源
     data: [],
     imgHeight: 176,
     // 控制自动播放属性
-    isPlay: false
+    isPlay: false,
+    // 顶部关键字
+    keyword: ''
   }
   componentDidMount() {
-    this.getSwiperData()
+    // this.getSwiperData()
+    // this.getGroupData()
+    // this.getNewsData()
+    // this.getAllData()
   }
+  // Promise.all 封装获取接口数据方法
+  // getAllData = async () => {
+  //   try {
+  //     const methods = [getSwiper(), getGroup(), getNews()];
+  //     let res = await Promise.all(methods);
+  //     // console.log('all datas:', res);
+  //     this.setState({
+  //       data: res[0].data,
+  //       groupListData: res[1].data,
+  //       news: res[2].data
+  //     }, () => {
+  //       this.setState({
+  //         aplay: true
+  //       })
+  //     })
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+
+  // }
+  // 获取走马灯数据
   getSwiperData = async () => {
     const { status, data } = await getSwiper()
     // console.log(data)
@@ -30,6 +60,7 @@ class Index extends React.Component {
       })
     }
   }
+  // 渲染走马灯ui
   renderCarousel = () => {
     return (
       // 走马灯
@@ -61,25 +92,135 @@ class Index extends React.Component {
 
     )
   }
+  // flex 布局展示
   showFlex = () => {
-    return(
+    return (
       <Flex className="nav">
         {
           navs.map((item) => <Flex.Item onClick={() => this.props.history.push(item.path)} key={item.id}>
-          <img src={item.img} alt=""/>
-          <p>{item.title}</p>
-        </Flex.Item>)
+            <img src={item.img} alt="" />
+            <p>{item.title}</p>
+          </Flex.Item>)
         }
       </Flex>
     )
   }
+  // 获取宫格小组数据
+  getGroupData = async () => {
+    const { status, data } = await getGroup()
+    // console.log(data)
+    if (status === 200) {
+      this.setState({
+        groupListData: data
+      })
+    }
+  }
+  // 渲染宫格小组ui 空标签的使用
+  showGroup = () => {
+    return (
+      <>
+        <Flex className="group-title" justify="between">
+          <h3>租房小组</h3>
+          <span>更多</span>
+        </Flex>
+        <Grid
+          data={this.state.groupListData}
+          columnNum={2}
+          // 关闭默认正方形
+          square={false}
+          hasLine={false}
+          renderItem={item => {
+            return (
+              // item结构
+              <Flex className="grid-item" justify="between">
+                <div className="desc">
+                  <h3>{item.title}</h3>
+                  <p>{item.desc}</p>
+                </div>
+                <img src={`${BASE_URL}${item.imgSrc}`} alt="" />
+              </Flex>
+            )
+          }}
+        />
+      </>
+    )
+  }
+  // 渲染资讯列表UI
+  showNews() {
+    return this.state.news.map(item => (
+      <div className="news-item" key={item.id}>
+        <div className="imgwrap">
+          <img
+            className="img"
+            src={`${BASE_URL}${item.imgSrc}`}
+            alt=""
+          />
+        </div>
+        <Flex className="content" direction="column" justify="between">
+          <h3 className="title">{item.title}</h3>
+          <Flex className="info" justify="between">
+            <span>{item.from}</span>
+            <span>{item.date}</span>
+          </Flex>
+        </Flex>
+      </div>
+    ))
+  }
+  // 获取资讯列表数据
+  getNewsData = async () => {
+    const { status, data } = await getNews()
+    // console.log(data)
+    if (status === 200) {
+      this.setState({
+        news: data
+      })
+    }
+  }
+
+  // 渲染顶部搜索
+  showTopNav = () => {
+    return (
+      <Flex justify="around" className="topNav">
+        <div className="searchBox">
+          <div className="city" onClick={()=>{
+            this.props.history.push('/citylist')
+          }}>
+            北京<i className="iconfont icon-arrow" />
+          </div>
+          <SearchBar
+            value={this.state.keyword}
+            onChange={(v) => this.setState({ keyword: v })}
+            placeholder="请输入小区或地址"
+          />
+        </div>
+        <div className="map" onClick={()=>{
+          this.props.history.push('/map')
+        }}>
+          <i key="0" className="iconfont icon-map" />
+        </div>
+      </Flex>
+    )
+  }
+
   render() {
     return (
       <div>
+        {/* 顶部搜索 */}
+        {this.showTopNav()}
         {/* 走马灯 */}
         {this.renderCarousel()}
         {/* // Flex 布局 */}
         {this.showFlex()}
+        {/* 租房宫格展示 */}
+        <div className="group">
+          {this.showGroup()}
+        </div>
+        {/* 最新资讯 */}
+        <div className="news">
+          <h3 className="group-title">最新资讯</h3>
+          <WingBlank size="md">{this.showNews()}</WingBlank>
+        </div>
+        {/*  */}
       </div>
     )
   }
--- src/utils/api/Home/index.js
+++ src/utils/api/Home/index.js
@@ -5,3 +5,20 @@ import instance from '../../axios'
 export function getSwiper() {
     return instance.get('/home/swiper')
 }
+
+// 获取租房小组接口
+export function getGroup(area) {
+    return instance.get('/home/groups',{
+        params: {
+            area
+        }
+    })
+}
+// 获取资讯小组接口
+export function getNews(area) {
+    return instance.get('/home/news',{
+        params: {
+            area
+        }
+    })
+}
