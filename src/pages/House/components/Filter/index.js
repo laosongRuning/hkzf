ef4f18f9804ca2cfe3c5b85f7b9002e0dd2c4fac
laosongRuning <2621990795@qq.com>
2020-04-25 16:13:08 +0800

    列表找房获取筛选条件数据（封装接口，获取数据，解构数据后存到实例上）

--- src/pages/House/components/Filter/index.js
+++ src/pages/House/components/Filter/index.js
@@ -6,8 +6,9 @@ import FilterTitle from '../FilterTitle'
 import FilterPicker from '../FilterPicker'
 // 更多条件选择
 import FilterMore from '../FilterMore'
-
+import {getFilters} from '../../../../utils/api/House'
 import styles from './index.module.css'
+import { getCurCity } from '../../../../utils'
 
 // 过滤器的title默认的高亮状态
 const titleSelectedStatus = {
@@ -39,6 +40,32 @@ export default class Filter extends Component {
     return openType === 'area' || openType === 'mode' || openType === 'price'
   }
 
+  // 关闭前三个筛选器内容和遮罩层
+  onCancel = () => {
+    this.setState({
+      openType: ''
+    })
+  }
+
+  // 确定选择过滤条件
+  onOk = () => {
+    this.setState({
+      openType: ''
+    })
+  }
+
+
+  // 获取筛选条件的数据
+  getFilterData= async() => {
+    let curCity = await getCurCity()
+    let {value} = curCity
+    const res = await getFilters(value)
+    let {status,data} = res
+    if(status === 200) {
+      this.filterData = data
+    }
+
+  }
 
   render() {
     return (
@@ -49,14 +76,14 @@ export default class Filter extends Component {
         <div className={styles.content}>
           {/* 前三个菜单的遮罩层 */}
           {
-            this.isShow() ? <div className={styles.mask} /> : null
+            this.isShow() ? <div className={styles.mask}  onClick={this.onCancel}/> : null
           }
           {/* 标题栏 */}
           <FilterTitle onTitleClick={this.onTitleClick} titleSelectedStatus={this.state.titleSelectedStatus} />
 
           {/* 前三个菜单对应的内容： */}
           {
-            this.isShowPicker ? <FilterPicker /> : null
+            this.isShowPicker ? <FilterPicker onCancel={this.onCancel} onOk={this.onOk} /> : null
           }
 
           {/* 最后一个菜单对应的内容： */}
--- src/utils/api/House/index.js
+++ src/utils/api/House/index.js
@@ -1,2 +1,11 @@
 // 找房所有的接口
 // import instance from '../../axios'
+import instance from '../../axios'
+
+export function getFilters(id) {
+    return instance.get('/house/condition',{
+        params: {
+            id
+        }
+    })
+}
