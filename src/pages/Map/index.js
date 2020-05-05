50b55ac412565b89a9161cb79ea650399d5733bb
laosongRuning <2621990795@qq.com>
2020-05-05 10:41:38 +0800

    地图找房-创建文本覆盖物

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './index.scss'
 import { NavBar, Icon } from 'antd-mobile';
-import {getCurCity} from '../../utils/index'
+import { getCurCity } from '../../utils/index'
 class Map extends React.Component {
     componentDidMount() {
         this.initMap()
     }
     // 初始化地图
-    initMap = async() => {
+    initMap = async () => {
         // 创建地图实例
         //  地图定位的经纬度设置(天安门)
         // 地图初始化，同时设置地图展示级别
@@ -28,8 +28,28 @@ class Map extends React.Component {
                 map.addControl(new BMap.NavigationControl())
                 map.addControl(new BMap.ScaleControl())
                 // map.addOverlay(new BMap.Marker(point));
+                // 创建文本覆盖物
+                const opts = {
+                    position: point, // 指定文本标注所在的地理位置
+                    offset: new BMap.Size(0, 0) //设置文本偏移量
+                }
+                // 初始化覆盖物实例
+                const label = new BMap.Label(
+                    '欢迎使用百度地图，这是一个简单的文本标注哦~',
+                    opts
+                ) 
+                // 创建文本标注对象
+                label.setStyle({
+                    color: 'red',
+                    fontSize: '12px',
+                    height: '20px',
+                    lineHeight: '20px',
+                    fontFamily: '微软雅黑'
+                })
+                // 调用百度地图实例的addOverlay=》 添加覆盖物到地图
+                map.addOverlay(label)
             }
-        },lable)
+        }, lable)
     }
 
     render() {
